cfa41268a756b558cdf70c377eab1f9b9aeb8b0c
M.D.W.De Silva <wdulanjith@gmail.com>
2024-11-15 13:20:36 +0530

    input component updated

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -1,10 +1,10 @@
-export default function Input({ label, type }) {
+export default function Input({ label, ...props }) {
   return (
     <p className="flex flex-col mb-4">
       <label className="mb-2 text-gray-700 font-semibold">{label}</label>
       <input
         className="px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-blue-500"
-        type={type}
+        {...props}
       />
     </p>
   );
